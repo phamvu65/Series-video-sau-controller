a2dfe6e4660d46e6c688884cf8e2f2ade3ab4b6d
Pham Vu <phamvu65cs@gmail.com>
2025-06-02 21:27:53 +0700

    buoi1

--- src/main/java/com/laptrinhjavaweb/api/BuildingAPI.java
+++ src/main/java/com/laptrinhjavaweb/api/BuildingAPI.java
@@ -3,20 +3,14 @@ package com.laptrinhjavaweb.api;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.deser.BuilderBasedDeserializer;
 import com.laptrinhjavaweb.bean.BuildingBean;
 
 //@Controller
@@ -35,7 +29,8 @@ public class BuildingAPI {
 	}
 
 	@PostMapping("/api/building")
-	public BuildingBean createBuilding(@RequestBody BuildingBean newBuilding){		
+	public BuildingBean createBuilding(@RequestBody BuildingBean newBuilding){	
+		System.out.println("ok");
 		return null;
 	}
 	
